e61f3383d47452a390e40cc1e61cfc16b00b7c5e
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-14 09:59:53 +0000

    Enter a name

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-try:
-  print("Enter a number between 1 and 10:")
-  number = int(input())
-except ValueError:
-  print("You must enter a number between 1 and 10:")
-  number = int(input())
-  if number >= 1 and number <= 10 :
-    not_validated = False
-  else:
-    print("Number entered out of range")
-    print("Enter a number between 1 and 10:")
-    number = int(input())
+print("Enter a name:")
+name = input()
+while name == "":
+  print("Please enter a name:")
+  name = input()
+print(f"Stored name: {name}")
