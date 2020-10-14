aecd230ae50a07165b879e201a670412842e0dee
Mathman <17e23052@marling.gloucs.sch.uk>
2020-10-14 09:30:31 +0000

    A2 v1.0

---
+++ main.py
@@ -0,0 +1,10 @@
+try:
+  print("Enter a number between 1 and 10:")
+  number = int(input())
+except ValueError:
+  print("You must enter a number between 1 and 10:")
+  number = int(input())
+if number >= 1 and number <= 10 :
+    not_validated = False
+else:
+    print("Number entered out of range")
